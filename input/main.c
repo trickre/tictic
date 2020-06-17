fc5d097c996618fd6deb8c131b18b00934d867ba
tricker <ogino.ktai@gmail.com>
2020-06-17 21:04:30 +0900

    initial commit

--- input/main.c
+++ input/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+static int var1 ;
+static int is_on ;
+
+void Interval_func();
+
 int main(){
-  printf("Hello world\n") ;
+  int i = 0;
+
+  for (i ;i<100;i++){
+    Interval_func() ;
+  }
 }
 
+void Interval_func(){
+  
+  if(is_on){
+    var1++ ;
+  }
+  
+}
